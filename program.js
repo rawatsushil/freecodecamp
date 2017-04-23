dca867b7ee20f9c91c38e7bb740bd972d6487a6f
sushil <sushil.rawat01@outlook.com>
2017-04-23 10:39:39 +0000

    first commit

---
+++ program.js
@@ -0,0 +1,14 @@
+var filepath = process.argv[2];
+
+var fs= require("fs");
+fs.readFile(filepath,function(err,data){
+    
+    if(err){
+        console.log("error"+err);
+    }
+    else
+        var array = data.toString().split("\n");
+        console.log(array.length-1);
+        
+    
+});
